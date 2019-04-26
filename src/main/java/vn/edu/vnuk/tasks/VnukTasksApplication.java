cbbb77741460ac70ea732b7996597c4485595398
ThanhQuang <tranthanhquang47@gmail.com>
2019-04-26 11:01:07 +0700

    ss

---
+++ src/main/java/vn/edu/vnuk/tasks/VnukTasksApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.vnuk.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VnukTasksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VnukTasksApplication.class, args);
+	}
+
+}
